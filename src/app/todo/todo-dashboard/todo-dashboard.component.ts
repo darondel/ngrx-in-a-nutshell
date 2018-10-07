dabf1edfd3e662996143ceafc0e766e4fa1c6351
Damien Arondel <damien.arondel@icloud.com>
2018-10-07 22:18:23 +0200

    Set up the Todo Dashboard component (display all the Todo components)

---
+++ src/app/todo/todo-dashboard/todo-dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-dashboard',
+  templateUrl: './todo-dashboard.component.html',
+  styleUrls: ['./todo-dashboard.component.css']
+})
+export class TodoDashboardComponent {
+}
