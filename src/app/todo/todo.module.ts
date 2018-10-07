14eee8507808bae2defecf363e917509990e5914
Damien Arondel <damien.arondel@icloud.com>
2018-10-07 22:18:47 +0200

    Wrap the Todo feature into a module

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule, MatFormFieldModule, MatGridListModule, MatInputModule, MatListModule } from '@angular/material';
+
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoCounterComponent } from './todo-counter/todo-counter.component';
+import { TodoStateComponent } from './todo-state/todo-state.component';
+import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
+    MatListModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    TodoCounterComponent,
+    TodoDashboardComponent,
+    TodoFormComponent,
+    TodoListComponent,
+    TodoStateComponent
+  ],
+  exports: [
+    TodoDashboardComponent
+  ]
+})
+export class TodoModule {
+}
