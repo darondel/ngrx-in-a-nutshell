5ad4d96ba20e9cae5e3f0571c33808d3a5b5cf00
Damien Arondel <damien.arondel@icloud.com>
2018-10-11 15:53:24 +0200

    Set up the Redux Principles component

---
+++ src/app/redux/redux-principles/redux-principles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redux-principles',
+  templateUrl: './redux-principles.component.html',
+  styleUrls: ['./redux-principles.component.css']
+})
+export class ReduxPrinciplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
