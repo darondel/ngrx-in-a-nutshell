3907cdc46c10dae9f33e88a08747f78853d93f6f
Damien Arondel <damien.arondel@icloud.com>
2018-10-12 12:03:48 +0200

    Bind to connect the Angular Router to NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
 
 import { CoreModule } from './core/core.module';
@@ -24,6 +25,7 @@ import { environment } from '../environments/environment';
     StoreDevtoolsModule.instrument({
       logOnly: environment.production
     }),
+    StoreRouterConnectingModule.forRoot(),
     EffectsModule.forRoot([TodoEffects]),
     CoreModule
   ],
