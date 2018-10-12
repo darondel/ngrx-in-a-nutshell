fb8fbb785ee953cd5fe8fbc46f2406f61c381cf6
Damien Arondel <damien.arondel@icloud.com>
2018-10-12 12:12:13 +0200

    Update the Redux Showcase component to use the nested Section one

--- src/app/redux/redux-showcase/redux-showcase.component.ts
+++ src/app/redux/redux-showcase/redux-showcase.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ReduxSection } from '../store/models/redux.model';
+
 @Component({
   selector: 'app-redux-showcase',
   templateUrl: './redux-showcase.component.html',
   styleUrls: ['./redux-showcase.component.css']
 })
 export class ReduxShowcaseComponent {
+
+  ReduxSection = ReduxSection;
+
 }
